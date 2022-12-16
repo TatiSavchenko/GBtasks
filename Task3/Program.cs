58ee3f70e1073339dfca3954bbe2cf08ea69d28a
TatianaSavchenko <tania95044519@mail.ru>
2022-12-16 16:18:17 -0300

    Solver Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Enter a number: ");
+
+int day = int.Parse(ReadLine()!);
+
+if(day <= 7 && day >= 1)
+{
+    if(day == 3)
+    {
+        WriteLine("Wednesday");
+    }
+
+    if(day == 5)
+    {
+        WriteLine("Friday");
+    }
+
+    if(day == 1)
+    {
+        WriteLine("Monday");
+    }
+
+    if(day == 2)
+    {
+        WriteLine("Tuesday");
+    }
+
+    if(day == 4)
+    {
+        WriteLine("Thursday");
+    }
+}
+else
+{
+    WriteLine("Such day does not exist");
+}
