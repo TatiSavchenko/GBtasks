afee462cb1ce7add822d6bf27ac0c891945203d2
TatianaSavchenko <tania95044519@mail.ru>
2022-12-17 15:46:11 -0300

    Solbed Task4 Homework

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number 1: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Enter a number 2: ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine("Enter a number 3: ");
+int c = int.Parse(ReadLine()!);
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+
+if(c > max)
+{
+    max = c;
+}
+
+Write($"Maximum is {max}");
+
+
+
+
+
