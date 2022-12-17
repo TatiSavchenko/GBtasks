251dc9dd6503d1ba44a14716651ed90c3f689ef1
TatianaSavchenko <tania95044519@mail.ru>
2022-12-16 21:49:56 -0300

    Solved Tasks1-1

--- Task1-1/Program.cs
+++ Task1-1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using static System.Console;
+Clear();
+WriteLine("Enter a number: ");
+int number = int.Parse(ReadLine()!);
+
+if(number >=100 && number < 1000)
+{
+    Write("The last digit is: ");
+    Write(number%10);
+}
+else
+{
+    WriteLine("Enter other number of xxx");
+    return;
+}
