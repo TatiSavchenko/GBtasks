74e18ce0082d0e3e0f1b07e241b3e48885d50e1d
TatianaSavchenko <tania95044519@mail.ru>
2022-12-17 12:57:02 -0300

    Solved Task6 Homework

--- Homework/Task6/Program.cs
+++ Homework/Task6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Enter a number: ");
+int n = int.Parse(ReadLine()!);
+
+if(n%2 == 0)
+{
+    WriteLine("YES, the number is even");
+}
+else
+WriteLine("NO");
