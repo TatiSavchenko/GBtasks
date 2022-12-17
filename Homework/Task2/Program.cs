0a182baab9089b29bf30b81dad7bb0ff9b5d4a43
TatianaSavchenko <tania95044519@mail.ru>
2022-12-16 22:45:38 -0300

    Solved Task2 Homework

--- Homework/Task2/Program.cs
+++ Homework/Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Enter number 1: ");
+int n1 = int.Parse(ReadLine()!);
+
+Write("Enter number 2: ");
+int n2 = int.Parse(ReadLine()!);
+
+if(n1 > n2)
+{
+    Write($"Max {n1} ");
+
+    Write($"Min {n2} ");
+}
+
+if(n1 < n2)
+{
+    Write($"Max {n2} ");
+
+    Write($"Min {n1} ");
+}
+else
+{
+    Write($"{n1} = {n2}");
+}
+
