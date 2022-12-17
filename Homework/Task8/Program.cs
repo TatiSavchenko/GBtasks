0bd6fee445938cd7725c542c533dc0e8de7a05fe
TatianaSavchenko <tania95044519@mail.ru>
2022-12-17 14:50:35 -0300

    Solved Task8 Homework

--- Homework/Task8/Program.cs
+++ Homework/Task8/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Enter a number: ");
+int N = int.Parse(ReadLine()!);
+int a = 1;
+
+while(a < N)
+{
+    a+=1;
+
+    if(a%2==0)
+    {
+        Write($" {a} ");
+    };
+}
+
+
