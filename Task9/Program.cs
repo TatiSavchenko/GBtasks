d75fb13ce324eb5008783af795ce664cc24b9e0a
TatianaSavchenko <tania95044519@mail.ru>
2022-12-20 09:47:43 -0300

    Solved Task9

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+int number=new Random().Next(10, 100);
+WriteLine(number);
+
+WriteLine(number%2==0?"Even":"Odd"); // проверка на четность с тернарным оператором
+
+int max = number/10 > number%10?number/10:number%10; // тернарный оператор int max=a1>a2?a1:a2; избавились от переменных
+
+WriteLine(max); // WriteLine(a1>a2?a1:a2);
+
+// if(a1>a2)
+// {
+//    WriteLine(a1);
+// }
+// else
+// {
+//    WriteLine(a2);
+//}
