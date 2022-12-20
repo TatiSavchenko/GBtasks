8f138675b3201332f0307d83641df57e3c65ad8c
TatianaSavchenko <tania95044519@mail.ru>
2022-12-20 09:45:53 -0300

    Solved Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number 1: ");
+int number1 = int.Parse(ReadLine()!);
+
+WriteLine("Enter a number2: ");
+int number2 = int.Parse(ReadLine()!);
+
+
+WriteLine(number1%number2==0?"Multiple":$"Not multiple, remainder is {number1%number2}");
+
+WriteLine(number1==number2?"=":number1>number2? ">":"<");
+
+
+
+
+
+
+
