8b885a86bf4880448a38e830ffca45c8fba84eb9
TatianaSavchenko <tania95044519@mail.ru>
2022-12-20 15:41:21 -0300

    Solved Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number 1: ");
+int number1 = Convert.ToInt32(ReadLine());
+
+WriteLine("Enter a number 2: ");
+int number2 = Convert.ToInt32(ReadLine());
+
+WriteLine(number1 == number2*number2?$"{number1} is a square of {number2}": 
+number2==number1*number1?$"{number2} is a square of {number1}":"NO");
