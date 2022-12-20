af39094cb3ac8702083ef6913c50daa02cc2a87e
TatianaSavchenko <tania95044519@mail.ru>
2022-12-20 18:30:23 -0300

    Solved Task10 Homework

--- Homework/Task10/Program.cs
+++ Homework/Task10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a three-digit numbrer: ");
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine(number>100 && number <1000?number/10%10:
+"Enter other number corresponding to the condition");
+
+// if(number >100 && number <1000)
+// {
+//    WriteLine($"{number/10%10}");
+// }
+// else
+// {
+//    WriteLine("Enter other number corresponding to the condition!");
+//}
+
