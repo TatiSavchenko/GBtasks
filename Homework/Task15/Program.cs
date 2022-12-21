c2ca7626e176950209752eb57b9a493b5a6d8553
TatianaSavchenko <tania95044519@mail.ru>
2022-12-20 22:05:14 -0300

    Solved Task15 Homework

--- Homework/Task15/Program.cs
+++ Homework/Task15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+WriteLine("Enter a number of the day of the week: ");
+int day = Convert.ToInt32(ReadLine());
+
+if(day<=7 && day>=1)
+{
+    WriteLine(day==7 || day==6? "Weekend!":"Work day!");
+}
+else
+{
+    WriteLine("Enter a number corresponding to the condition!");
+}
