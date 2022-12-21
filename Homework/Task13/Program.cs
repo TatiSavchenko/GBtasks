bdc681cdb3e5762eaa65a89f948daefce0b75b58
TatianaSavchenko <tania95044519@mail.ru>
2022-12-20 23:47:22 -0300

    solved task13 homeworl

--- Homework/Task13/Program.cs
+++ Homework/Task13/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number: ");
+int number = Convert.ToInt32(ReadLine());
+
+
+while(number>999)
+{    
+    number = number/10; //продолжаем делить на 10
+    
+
+}
+
+WriteLine(number<100?"Third digil does not exist":$"{number%10}");
+
+
