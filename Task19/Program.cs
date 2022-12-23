e2efa757eff43c78669a64626ae7a859f27b2d72
TatianaSavchenko <tania95044519@mail.ru>
2022-12-23 13:12:27 -0300

    Solved Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter x: ");
+int x = int.Parse(ReadLine()!);
+
+WriteLine("Enter y: ");
+int y = int.Parse(ReadLine()!);
+
+if(x>0)
+{
+    if(y>0)
+    {
+        WriteLine("First quarter");
+        return;
+    }
+    if(y<0)
+    {
+        WriteLine("Forth quarter");
+        return;
+    }
+}
+if(x<0)
+{
+    if(y>0)
+    {
+        WriteLine("Second quarter");
+        return;
+    }
+    if(y<0)
+    {
+        WriteLine("Third quarter");
+        return;
+    }
+}
+if(x==0 || y==0)
+{
+    WriteLine("Error");
+    return;
+}
