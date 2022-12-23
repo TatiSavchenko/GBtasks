c31e979d5ba8c254d54ef27113a7c90e7266a389
TatianaSavchenko <tania95044519@mail.ru>
2022-12-23 12:48:38 -0300

    Solved Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number of the day of week: ");
+int day = int.Parse(ReadLine()!);
+
+switch(day)
+{
+    case 1:
+    {
+        WriteLine("Monday");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("Tuesday");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("Wednesday");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("Thursday");
+        break;
+    }
+    case 5:
+    {
+        WriteLine("Friday");
+        break;
+    }
+    case 6:
+    {
+        WriteLine("Saturday");
+        break;
+    }
+    case 7:
+    {
+        WriteLine("Sunday");
+        break;
+    }
+    default:
+    {
+        WriteLine("Error");
+        break;
+    }
+}
