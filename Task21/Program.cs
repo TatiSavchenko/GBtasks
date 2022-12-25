34c0b89c3c4815c989bb8917a3f000e2d46d6c75
TatianaSavchenko <tania95044519@mail.ru>
2022-12-25 17:31:43 -0300

    Solved Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+Write("Enter a number from 1 to N: ");
+int.TryParse(ReadLine(),out int N);
+int a = 0;
+
+while(a <= N)
+{
+    Write($"{Math.Pow(a,2)} | ");
+    a = a+1;
+}
