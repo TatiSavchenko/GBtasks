eac44d3b54d0457f5e7ff40bc12280f76ab624de
TatianaSavchenko <tania95044519@mail.ru>
2022-12-25 18:51:45 -0300

    Solved Task19 Homework

---
+++ Homework/Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a five-digit number: ");
+int.TryParse(ReadLine(),out int N);
+
+if(N <= 99999 && N >=10000)
+{
+    int a = N%100; int b = N/1000;
+    
+    Write(a/10 == b%10 && a%10 == b/10?"Yes, the number is Palindrome":"NO, the number is not Palindrome");
+}
+else
+{
+    WriteLine("Enter a five-digit number!");
+}
+
