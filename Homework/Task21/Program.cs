b13c3165500b2d5985e5716650fc450b5c2b1d25
TatianaSavchenko <tania95044519@mail.ru>
2022-12-25 23:27:39 -0300

    Solved Task21 Homework

--- Homework/Task21/Program.cs
+++ Homework/Task21/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+
+Write("Point A -> Enter x: ");
+int.TryParse(ReadLine(),out int ax);
+
+Write("Point A -> Enter y: ");
+int.TryParse(ReadLine(),out int ay);
+
+Write("Point A -> Enter z: ");
+int.TryParse(ReadLine(),out int az);
+
+Write("Point B -> Enter x: ");
+int.TryParse(ReadLine(),out int bx);
+
+Write("Point B -> Enter y: ");
+int.TryParse(ReadLine(),out int by);
+
+Write("Point B -> Enter z: ");
+int.TryParse(ReadLine(),out int bz);
+
+double length = Math.Sqrt(Math.Pow((ax-bx),2)+Math.Pow((ay-by),2)+Math.Pow((az-bz),2));
+WriteLine($"{length:f2}");
