4c9efccefc7d74cefc431a72efa746cfca96417a
TatianaSavchenko <tania95044519@mail.ru>
2022-12-26 10:10:51 -0300

    Solved Task23 Homework

--- Homework/Task23/Program.cs
+++ Homework/Task23/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Enter a number: ");
+int.TryParse(ReadLine(),out int N);
+
+int a = 0;
+while(a <= N)
+{
+    Write($"{Math.Pow(a,3)} | ");
+    a++;
+}
