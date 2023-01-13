c7168e094f14f90ee8e696d84e9a3bd656f631e3
TatianaSavchenko <tania95044519@mail.ru>
2023-01-13 12:47:55 -0300

    Solved task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sum = GetSum(number);
+Console.WriteLine($"Sum number 1...A = {sum}");
+
+int GetSum(int A)
+{
+    int result = 0;
+    while(A>0)
+    {
+        result+=A;
+        A--;
+    }
+    return result;
+}
