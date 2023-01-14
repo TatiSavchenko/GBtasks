b3880f2a16ca993878cbeeac56b4f54a333a95a1
TatianaSavchenko <tania95044519@mail.ru>
2023-01-14 11:45:11 -0300

    Solved Task27Homework

--- Homework/Task27/Program.cs
+++ Homework/Task27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую метод, который принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits of number = {Sum(n)}");
+
+int Sum(int N)
+{
+    int result = 0;
+
+    while(N>0)
+    {
+        int digit = N%10;
+        result=result+digit;
+
+        N = N/10;
+    }
+    return result;
+}
