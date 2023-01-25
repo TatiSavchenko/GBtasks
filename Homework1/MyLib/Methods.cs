e77e93b4ac22ce389bff44f81d882c0f53aa1883
TatianaSavchenko <tania95044519@mail.ru>
2023-01-25 19:12:19 -0300

    Added methods

--- Homework1/MyLib/Methods.cs
+++ Homework1/MyLib/Methods.cs
@@ -1,6 +1,9 @@
 ﻿namespace MyLib;
 public class Methods
 {
+    ///<summary>
+    ///Method 1
+    ///</summary>
     public static int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
 {
     Random rnd = new Random();
@@ -14,4 +17,38 @@ public class Methods
     }
     return ArrayMatrix;
 }
+
+
+    ///<summary>
+    ///Method 2 MatrixArrayDouble
+    ///</summary>
+    public static double[,] GetMatrixArray(int rows, int columns)
+{
+    Random rnd = new Random();
+    double [,]ArrayMatrix = new double[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j=0; j<columns; j++)          
+        {
+            ArrayMatrix[i,j] = rnd.NextDouble()*100;
+        }
+    }
+    return ArrayMatrix;
+}
+    ///<summary>
+    ///Method Double PrintMatrixArray
+    ///</summary>
+    public static void PrintMatrixArray(double[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j],8:f2} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
 }
