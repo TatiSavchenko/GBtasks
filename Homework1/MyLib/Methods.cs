b005aeda947ee6b8d4accd9fdabfd14ef6567d50
TatianaSavchenko <tania95044519@mail.ru>
2023-01-25 17:43:18 -0300

    Add MyLib

---
+++ Homework1/MyLib/Methods.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLib;
+public class Methods
+{
+    public static int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int [,]ArrayMatrix = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j=0; j<columns; j++)          
+        {
+            ArrayMatrix[i,j] = rnd.Next(minValue, maxValue+1);
+        }
+    }
+    return ArrayMatrix;
+}
+}
