35c040ac40bf995fb62c62f068c6275c5c5642f4
TatianaSavchenko <tania95044519@mail.ru>
2023-01-31 18:26:18 -0300

    Solved Task68_Homework

---
+++ Homework1/Task68/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 68: Напишите программу реализующую метод вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int resAkk = Akkerman(2,3);
+Console.WriteLine(resAkk);
+Console.WriteLine();
+int resAkk1 = Akkerman(3,2);
+Console.WriteLine(resAkk1);
+
+
+int Akkerman (int m, int n)
+{
+    if(m==0) return n+1;
+    else
+    {
+        if(m!=0 && n==0)
+        {
+            return Akkerman(m-1,1);
+        }
+        else
+        {
+            return Akkerman(m-1, Akkerman(m,n-1));
+        }   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
