6e21b13ddda8166b1cdafd9d28cb939f065cad18
TatianaSavchenko <tania95044519@mail.ru>
2023-01-31 17:48:20 -0300

    Solved Task64_Homework

---
+++ Homework1/Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Напишите программу,
+//  которая реализует рекурсивный метод вывода всех натуральных числел в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string numbers = GetNumbers(10);
+Console.WriteLine(numbers);
+
+string GetNumbers(int num)
+{
+    if(num == 1) return num.ToString();
+    else
+    {
+        return $"{num}, {GetNumbers(num-1)} ";
+    }
+}
