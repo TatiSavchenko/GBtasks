f9fd699d83f107807abb64db93ae7d520d105931
TatianaSavchenko <tania95044519@mail.ru>
2023-01-31 17:57:53 -0300

    Solved Task66_Homework

---
+++ Homework1/Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Напишите программу, 
+// которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int result = GetSum(1, 15);
+Console.WriteLine(result);
+
+int GetSum (int M, int N)
+{
+    if(M == N ) return N;
+    else
+    {
+        return GetSum(M+1, N)+ M;
+    }
+}
